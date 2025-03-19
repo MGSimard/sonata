aef07ca95e49e51c2c1154dfaab54f25a6efb923
MGSimard <mgsimard.dev@gmail.com>
2025-03-19 02:32:47 -0400

    This is a better description

--- src/_utils/maps.ts
+++ src/_utils/maps.ts
@@ -1,8 +1,8 @@
 // Define MIDI note numbers for each note
-type MidiNote = number;
+type NoteIndex = number;
 
 // Map from MIDI note number to note name
-const midiToNoteName: Record<MidiNote, string> = {
+const noteIndexToNote: Record<NoteIndex, string> = {
   0: "C1",
   1: "C#1",
   2: "D1",
@@ -79,8 +79,8 @@ const midiToNoteName: Record<MidiNote, string> = {
 };
 
 // Get a note name from a MIDI note number with optional transposition
-export function getNoteName(midiNote: MidiNote, transpose = 0): string | undefined {
-  return midiToNoteName[midiNote + transpose];
+export function getNoteName(noteIndex: NoteIndex, transpose = 0): string | undefined {
+  return noteIndexToNote[noteIndex + transpose];
 }
 
 // Combined type definition for keyboard mapping
@@ -88,7 +88,7 @@ type KeyMap = Record<
   string,
   Array<{
     char: string;
-    midiNote: MidiNote;
+    noteIndex: NoteIndex;
   }>
 >;
 
@@ -120,136 +120,136 @@ type KeyMap = Record<
  * */
 export const keyMap: KeyMap = {
   Digit1: [
-    { char: "1", midiNote: 12 }, // C2
-    { char: "!", midiNote: 13 }, // C#2
+    { char: "1", noteIndex: 12 }, // C2
+    { char: "!", noteIndex: 13 }, // C#2
   ],
   Digit2: [
-    { char: "2", midiNote: 14 }, // D2
-    { char: "@", midiNote: 15 }, // D#2
+    { char: "2", noteIndex: 14 }, // D2
+    { char: "@", noteIndex: 15 }, // D#2
   ],
   Digit3: [
-    { char: "3", midiNote: 16 }, // E2
+    { char: "3", noteIndex: 16 }, // E2
   ],
   Digit4: [
-    { char: "4", midiNote: 17 }, // F2
-    { char: "$", midiNote: 18 }, // F#2
+    { char: "4", noteIndex: 17 }, // F2
+    { char: "$", noteIndex: 18 }, // F#2
   ],
   Digit5: [
-    { char: "5", midiNote: 19 }, // G2
-    { char: "%", midiNote: 20 }, // G#2
+    { char: "5", noteIndex: 19 }, // G2
+    { char: "%", noteIndex: 20 }, // G#2
   ],
   Digit6: [
-    { char: "6", midiNote: 21 }, // A2
-    { char: "^", midiNote: 22 }, // A#2
+    { char: "6", noteIndex: 21 }, // A2
+    { char: "^", noteIndex: 22 }, // A#2
   ],
   Digit7: [
-    { char: "7", midiNote: 23 }, // B2
+    { char: "7", noteIndex: 23 }, // B2
   ],
   Digit8: [
-    { char: "8", midiNote: 24 }, // C3
-    { char: "*", midiNote: 25 }, // C#3
+    { char: "8", noteIndex: 24 }, // C3
+    { char: "*", noteIndex: 25 }, // C#3
   ],
   Digit9: [
-    { char: "9", midiNote: 26 }, // D3
-    { char: "(", midiNote: 27 }, // D#3
+    { char: "9", noteIndex: 26 }, // D3
+    { char: "(", noteIndex: 27 }, // D#3
   ],
   Digit0: [
-    { char: "0", midiNote: 28 }, // E3
+    { char: "0", noteIndex: 28 }, // E3
   ],
   q: [
-    { char: "q", midiNote: 29 }, // F3
-    { char: "Q", midiNote: 30 }, // F#3
+    { char: "q", noteIndex: 29 }, // F3
+    { char: "Q", noteIndex: 30 }, // F#3
   ],
   w: [
-    { char: "w", midiNote: 31 }, // G3
-    { char: "W", midiNote: 32 }, // G#3
+    { char: "w", noteIndex: 31 }, // G3
+    { char: "W", noteIndex: 32 }, // G#3
   ],
   e: [
-    { char: "e", midiNote: 33 }, // A3
-    { char: "E", midiNote: 34 }, // A#3
+    { char: "e", noteIndex: 33 }, // A3
+    { char: "E", noteIndex: 34 }, // A#3
   ],
   r: [
-    { char: "r", midiNote: 35 }, // B3
+    { char: "r", noteIndex: 35 }, // B3
   ],
   t: [
-    { char: "t", midiNote: 36 }, // C4
-    { char: "T", midiNote: 37 }, // C#4
+    { char: "t", noteIndex: 36 }, // C4
+    { char: "T", noteIndex: 37 }, // C#4
   ],
   y: [
-    { char: "y", midiNote: 38 }, // D4
-    { char: "Y", midiNote: 39 }, // D#4
+    { char: "y", noteIndex: 38 }, // D4
+    { char: "Y", noteIndex: 39 }, // D#4
   ],
   u: [
-    { char: "u", midiNote: 40 }, // E4
+    { char: "u", noteIndex: 40 }, // E4
   ],
   i: [
-    { char: "i", midiNote: 41 }, // F4
-    { char: "I", midiNote: 42 }, // F#4
+    { char: "i", noteIndex: 41 }, // F4
+    { char: "I", noteIndex: 42 }, // F#4
   ],
   o: [
-    { char: "o", midiNote: 43 }, // G4
-    { char: "O", midiNote: 44 }, // G#4
+    { char: "o", noteIndex: 43 }, // G4
+    { char: "O", noteIndex: 44 }, // G#4
   ],
   p: [
-    { char: "p", midiNote: 45 }, // A4
-    { char: "P", midiNote: 46 }, // A#4
+    { char: "p", noteIndex: 45 }, // A4
+    { char: "P", noteIndex: 46 }, // A#4
   ],
   a: [
-    { char: "a", midiNote: 47 }, // B4
+    { char: "a", noteIndex: 47 }, // B4
   ],
   s: [
-    { char: "s", midiNote: 48 }, // C5
-    { char: "S", midiNote: 49 }, // C#5
+    { char: "s", noteIndex: 48 }, // C5
+    { char: "S", noteIndex: 49 }, // C#5
   ],
   d: [
-    { char: "d", midiNote: 50 }, // D5
-    { char: "D", midiNote: 51 }, // D#5
+    { char: "d", noteIndex: 50 }, // D5
+    { char: "D", noteIndex: 51 }, // D#5
   ],
   f: [
-    { char: "f", midiNote: 52 }, // E5
+    { char: "f", noteIndex: 52 }, // E5
   ],
   g: [
-    { char: "g", midiNote: 53 }, // F5
-    { char: "G", midiNote: 54 }, // F#5
+    { char: "g", noteIndex: 53 }, // F5
+    { char: "G", noteIndex: 54 }, // F#5
   ],
   h: [
-    { char: "h", midiNote: 55 }, // G5
-    { char: "H", midiNote: 56 }, // G#5
+    { char: "h", noteIndex: 55 }, // G5
+    { char: "H", noteIndex: 56 }, // G#5
   ],
   j: [
-    { char: "j", midiNote: 57 }, // A5
-    { char: "J", midiNote: 58 }, // A#5
+    { char: "j", noteIndex: 57 }, // A5
+    { char: "J", noteIndex: 58 }, // A#5
   ],
   k: [
-    { char: "k", midiNote: 59 }, // B5
+    { char: "k", noteIndex: 59 }, // B5
   ],
   l: [
-    { char: "l", midiNote: 60 }, // C6
-    { char: "L", midiNote: 61 }, // C#6
+    { char: "l", noteIndex: 60 }, // C6
+    { char: "L", noteIndex: 61 }, // C#6
   ],
   z: [
-    { char: "z", midiNote: 62 }, // D6
-    { char: "Z", midiNote: 63 }, // D#6
+    { char: "z", noteIndex: 62 }, // D6
+    { char: "Z", noteIndex: 63 }, // D#6
   ],
   x: [
-    { char: "x", midiNote: 64 }, // E6
+    { char: "x", noteIndex: 64 }, // E6
   ],
   c: [
-    { char: "c", midiNote: 65 }, // F6
-    { char: "C", midiNote: 66 }, // F#6
+    { char: "c", noteIndex: 65 }, // F6
+    { char: "C", noteIndex: 66 }, // F#6
   ],
   v: [
-    { char: "v", midiNote: 67 }, // G6
-    { char: "V", midiNote: 68 }, // G#6
+    { char: "v", noteIndex: 67 }, // G6
+    { char: "V", noteIndex: 68 }, // G#6
   ],
   b: [
-    { char: "b", midiNote: 69 }, // A6
-    { char: "B", midiNote: 70 }, // A#6
+    { char: "b", noteIndex: 69 }, // A6
+    { char: "B", noteIndex: 70 }, // A#6
   ],
   n: [
-    { char: "n", midiNote: 71 }, // B6
+    { char: "n", noteIndex: 71 }, // B6
   ],
   m: [
-    { char: "m", midiNote: 72 }, // C7
+    { char: "m", noteIndex: 72 }, // C7
   ],
 };
