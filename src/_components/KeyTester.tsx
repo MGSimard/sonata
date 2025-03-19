f291b8a01da79c2a1ce7dde960c3becef5c7f7e9
MGSimard <mgsimard.dev@gmail.com>
2025-03-18 20:49:16 -0400

    I think this makes sense

--- src/_components/KeyTester.tsx
+++ src/_components/KeyTester.tsx
@@ -9,9 +9,20 @@ import { noteMap, keyMap } from "@/_utils/maps";
  * e.key maps to mapped key (final output)
  */
 
-// If character is not a-z, refer to e.code
-// If character is a-z, refer to e.key?
-// This would give the greatest compatibility regarding both physical and digitally mapped keys
+/* PROBLEM
+ * - User's layout might not match their keyboard's physical layout (remapped keys, diff layouts)
+ * - Language layouts add an additional layer of complexity for SYMBOLS on number keys (6 -> ^ or ?)
+ */
+
+/* SOLUTION?
+ * - Since regardless of layout, numbers should be in the same spots, we can use e.code safely
+ * - Since letters might not match the keyboard's physical layout, we can use e.key for the actual character
+ * - This is safe because lowercase and uppercase versions of letter keys are always consistent
+ * - In short, use a hybrid method:
+ *   - If e.key is not A-Za-z, refer to e.code + e.shiftKey
+ *   - If e.key is A-Za-z, refer to e.key
+ * - This gives the greatest compatibility regarding both physical and digitally mapped keys
+ */
 
 export const KeyTester = () => {
   const transpose = 0; // -12 to +12
