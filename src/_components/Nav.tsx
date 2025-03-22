bd90c64e287b223783636c3598c321f11e3cc57f
MGSimard <mgsimard.dev@gmail.com>
2025-03-22 09:18:45 -0400

    Nav stuff

--- src/_components/Nav.tsx
+++ src/_components/Nav.tsx
@@ -1,19 +1,27 @@
 import { Link } from "@tanstack/react-router";
+import { useTheme } from "next-themes";
+import { IconSunSolid, IconSunOutline, IconInfo } from "@/_components/Icons";
 
 export function Nav() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <nav>
       <Link to="/" id="nav-logo">
         <img src="/metadata/icon.svg" alt="LOGO" /> <span>Sonata</span>
       </Link>
-      <ul id="nav-links">
-        <li>
-          <Link to="/">Play</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <div id="nav-controls">
+        <button type="button" aria-label="About" title="About">
+          <IconInfo />
+        </button>
+        <button
+          type="button"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+          title={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}>
+          {theme === "dark" ? <IconSunOutline /> : <IconSunSolid />}
+        </button>
+      </div>
     </nav>
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,16 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ThemeProvider } from "next-themes";
 import { Nav } from "@/_components/Nav";
 import { Footer } from "@/_components/Footer";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider disableTransitionOnChange={true} defaultTheme="dark">
       <Nav />
       <Outlet />
       <Footer />
       {/* <TanStackRouterDevtools /> */}
-    </>
+    </ThemeProvider>
   ),
 });
