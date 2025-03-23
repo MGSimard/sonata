0d0eaaccb8d043c111ad15615d0e40aab2ede040
MGSimard <mgsimard.dev@gmail.com>
2025-03-23 11:56:24 -0400

    a

--- src/_components/Piano.tsx
+++ src/_components/Piano.tsx
@@ -174,13 +174,13 @@ export const Piano = () => {
           </div>
         </div>
       </div>
+
       <div id="piano-keys-container">
         <div id="piano-slope"></div>
         <div id="piano-keys">
           {Object.entries(keyMap).map(([key, notes], keyIndex) => {
             const whiteNote = notes[0];
             const blackNote = notes[1];
-
             return (
               <Fragment key={key}>
                 {whiteNote && (
